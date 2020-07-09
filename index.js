494b675178610987184aa311c53071b80df209fb
Diane Candler <dianedcandler@gmail.com>
2020-07-09 12:06:05 -0500

    added inquirer

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
-const connnection = mysql.createConnection({
-host: 'localhost',
-port: 3306,
-user: 'root',
-password: 'password',
-database: 'employeeTracker'
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'password',
+	database: 'employeeTracker'
 });
 
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log('connected thread ID: ' + connection.threadId);
+
+	start();
+});
+
+function start () {
+	inquirer
+		.prompt({
+			type: 'list',
+			name: 'question',
+			message: 'what do you want to do?',
+			choices: [
+				'View departments?',
+				'View employee roles?',
+				'View employees?',
+				'Add department?',
+				'Add employee role?',
+				'Add employee?',
+				'Update employee role?'
+			]
+		})
+		.then(function (res) {
+			console.log(res);
+		});
+}
